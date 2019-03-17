81e6d6b7753d808f51b91ff5cacc422da3845774
Samuel Jackson <dsiq3g@gmail.com>
2019-03-16 23:58:53 -0700

    Split up emacs initilization into seprate files.
    Seporated the configurations for emacs so that the files were cleaner
    and easier to find things in. Still have possible reorganizing to do
    such as possible moving the package initialization config values to
    proper spots, but I haven't decided which is cleaner yet. For now this
    is much better than before.

---
+++ emacs.scripts/evil-mode_initialize.el
@@ -0,0 +1,6 @@
+;; Evil mode settings
+(setq evil-want-C-u-scroll t)
+
+  ; Must come at end
+(require 'evil)
+(evil-mode 1)
---
+++ emacs.scripts/org-mode_initialize.el
@@ -0,0 +1,5 @@
+;; Make Org mode work with files ending in .org
+(add-to-list 'auto-mode-alist '("\\.og$" . org-mode))
+
+;; Enable Org mode
+(require 'org)
