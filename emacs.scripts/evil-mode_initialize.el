6fb957584705207bcedef473b94a86a10fe1972b
Samuel Jackson <dsiq3g@gmail.com>
2019-03-17 00:28:56 -0700

    Added ability to use jk to exit evil insert mode.

--- emacs.scripts/evil-mode_initialize.el
+++ emacs.scripts/evil-mode_initialize.el
@@ -1,6 +1,12 @@
 ;; Evil mode settings
+(evil-mode 1)
+
 (setq evil-want-C-u-scroll t)
 
-  ; Must come at end
+;; Use JK as <esc>
+(evil-escape-mode 1)
+(setq-default evil-escape-key-sequence "jk")
+(setq-default evil-escape-delay 0.2)
+
+; Must come at end
 (require 'evil)
-(evil-mode 1)
