b1294b23d50741e18cb6415ec47a6990ce2ccaea
Samuel Jackson <dsiq3g@gmail.com>
2021-02-27 13:46:22 -0800

    Added zshrc.

---
+++ .shell_setup/env_var_setup.sh
@@ -0,0 +1,11 @@
+
+# Setting language parameter so it's consistent
+export LANG='en_US.UTF-8'
+
+# Preferred editor for local and remote sessions
+export EDITOR='vim'
+
+# ssh
+export SSH_KEY_PATH="${HOME}/.ssh/rsa_id"
+
+MANPATH="/usr/local/man:$MANPATH"
