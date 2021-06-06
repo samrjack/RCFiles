1e7bb28936ae5cbde3bd03f8b2e1c97429ba09fd
Samuel Jackson <dsiq3g@gmail.com>
2021-06-06 15:49:23 -0700

    Modified nyxt browser config for future usage.

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,3 +1,7 @@
-(define-configuration buffer
-    ((default-modes (append '(vi-normal-mode) %slot-defaults%))))
+(in-package #:nyxt-user)
 
+;; (dolist (file (list ())))
+(print (expand-path *init-file-path*))
+
+(define-configuration buffer
+    ((default-modes (append '(emacs-mode) %slot-default%))))
