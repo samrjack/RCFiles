4089692bfab1cc97191eecb8accb9a4dd3ee9d17
Samuel Jackson <dsiq3g@gmail.com>
2021-06-06 22:54:13 -0700

    Beginning to add personal capture notes.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -83,17 +83,80 @@ Position the cursor at it's beginning, according to the current mode."
 (setq org-directory "~/org/")
 (setq org-default-notes-file (concat org-directory "/inbox.org"))
 
-(setq org-pomodoro-length 25
-      org-pomodoro-short-break-length 5
-      org-pomodoro-long-break-length 15)
+;; Agenda
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/todo.org"))
 
-(setq org-pomodoro-play-sounds t
-      org-pomodoro-start-sound-p f
-      org-pomodoro-ticking-sound-p f
-      org-pomodoro-killed-sound-p t
-      org-pomodoro-finished-sound-p t
-      org-pomodoro-short-break-sound-p t
-      org-pomodoro-long-break-sound-p t)
+;; Example of org capture templates
+;; for example text, see https://github.com/hlissner/doom-emacs/blob/develop/modules/lang/org/config.el
+;; (after! org
+;;   (setq org-capture-templates '(
+;;     ("t" "Todo" entry () "" :prepend t)
+;;     ("k" "Kudos" entry () "" :prepend t)
+;;     ("f" "Followup" entry () "" :prepend t)
+;;     ("p" "Personal" entry () "" :prepend t)
+;;     ("P" "Project" entry () "" :prepend t)
+;;     ("j" "Journal" entry () "" :prepend t)
+;;   )))
+
+
+
+;; '(
+;;   ("t" "Personal todo" entry (file+headline +org-capture-todo-file "Inbox") "* [ ] %?\n%i\n%a" :prepend t)
+;;           ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox") "* %u %?\n%i\n%a" :prepend t)
+;;           ("j" "Journal" entry (file+olp+datetree +org-capture-journal-file)
+;;            "* %U %?\n%i\n%a" :prepend t)
+
+;;           ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
+;;           ;; {todo,notes,changelog}.org file is found in a parent directory.
+;;           ;; Uses the basename from `+org-capture-todo-file',
+;;           ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
+;;           ("p" "Templates for projects")
+;;           ("pt" "Project-local todo" entry  ; {project-root}/todo.org
+;;            (file+headline +org-capture-project-todo-file "Inbox")
+;;            "* TODO %?\n%i\n%a" :prepend t)
+;;           ("pn" "Project-local notes" entry  ; {project-root}/notes.org
+;;            (file+headline +org-capture-project-notes-file "Inbox")
+;;            "* %U %?\n%i\n%a" :prepend t)
+;;           ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
+;;            (file+headline +org-capture-project-changelog-file "Unreleased")
+;;            "* %U %?\n%i\n%a" :prepend t)
+
+;;           ;; Will use {org-directory}/{+org-capture-projects-file} and store
+;;           ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
+;;           ;; support `:parents' to specify what headings to put them under, e.g.
+;;           ;; :parents ("Projects")
+;;           ("o" "Centralized templates for projects")
+;;           ("ot" "Project todo" entry
+;;            (function +org-capture-central-project-todo-file)
+;;            "* TODO %?\n %i\n %a"
+;;            :heading "Tasks"
+;;            :prepend nil)
+;;           ("on" "Project notes" entry
+;;            (function +org-capture-central-project-notes-file)
+;;            "* %U %?\n %i\n %a"
+;;            :heading "Notes"
+;;            :prepend t)
+;;           ("oc" "Project changelog" entry
+;;            (function +org-capture-central-project-changelog-file)
+;;            "* %U %?\n %i\n %a"
+;;            :heading "Changelog"
+;;            :prepend t)))
+
+;; Pomodoro
+(after! org
+  (setq org-pomodoro-length 25
+        org-pomodoro-short-break-length 5
+        org-pomodoro-long-break-length 15))
+
+(after! org
+  (setq org-pomodoro-play-sounds t
+        ;; org-pomodoro-start-sound-p f
+        ;; org-pomodoro-ticking-sound-p f
+        org-pomodoro-killed-sound-p t
+        org-pomodoro-finished-sound-p t
+        org-pomodoro-short-break-sound-p t
+        org-pomodoro-long-break-sound-p t))
 
 ;; need to find (or make) some better alert audio files.
 ;; (setq ;org-pomodoro-start-sound ()
