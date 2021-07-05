35d8ceacccf405cc56f4708f2f9e4cc097e6c034
Samuel Jackson <dsiq3g@gmail.com>
2021-07-05 01:45:57 -0700

    Moved org-capture templates to new file.
    Also added the worry form template for addressing anxiety.

--- .doom.d/settings/org-settings.el
+++ .doom.d/settings/org-settings.el
@@ -49,66 +49,7 @@ if no org extension is given then it will be automatically appended."
       :desc "Find org file"
       "f o" #'org-open-org-file)
 
-(after! org
-  (add-to-list 'org-capture-templates
-                '("l" "Test Capture" checkitem (file+olp+datetree org-default-notes-file) "[ ]")))
 
-
-;; Example of org capture templates
-;; for example text, see https://github.com/hlissner/doom-emacs/blob/develop/modules/lang/org/config.el
-;; (after! org
-;;   (setq org-capture-templates '(
-;;     ("t" "Todo" entry () "" :prepend t)
-;;     ("k" "Kudos" entry () "" :prepend t)
-;;     ("f" "Followup" entry () "" :prepend t)
-;;     ("p" "Personal" entry () "" :prepend t)
-;;     ("P" "Project" entry () "" :prepend t)
-;;     ("j" "Journal" entry () "" :prepend t)
-;;   )))
-
-
-
-;; '(
-;;   ("t" "Personal todo" entry (file+headline +org-capture-todo-file "Inbox") "* [ ] %?\n%i\n%a" :prepend t)
-;;           ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox") "* %u %?\n%i\n%a" :prepend t)
-;;           ("j" "Journal" entry (file+olp+datetree +org-capture-journal-file)
-;;            "* %U %?\n%i\n%a" :prepend t)
-
-;;           ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
-;;           ;; {todo,notes,changelog}.org file is found in a parent directory.
-;;           ;; Uses the basename from `+org-capture-todo-file',
-;;           ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
-;;           ("p" "Templates for projects")
-;;           ("pt" "Project-local todo" entry  ; {project-root}/todo.org
-;;            (file+headline +org-capture-project-todo-file "Inbox")
-;;            "* TODO %?\n%i\n%a" :prepend t)
-;;           ("pn" "Project-local notes" entry  ; {project-root}/notes.org
-;;            (file+headline +org-capture-project-notes-file "Inbox")
-;;            "* %U %?\n%i\n%a" :prepend t)
-;;           ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
-;;            (file+headline +org-capture-project-changelog-file "Unreleased")
-;;            "* %U %?\n%i\n%a" :prepend t)
-
-;;           ;; Will use {org-directory}/{+org-capture-projects-file} and store
-;;           ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
-;;           ;; support `:parents' to specify what headings to put them under, e.g.
-;;           ;; :parents ("Projects")
-;;           ("o" "Centralized templates for projects")
-;;           ("ot" "Project todo" entry
-;;            (function +org-capture-central-project-todo-file)
-;;            "* TODO %?\n %i\n %a"
-;;            :heading "Tasks"
-;;            :prepend nil)
-;;           ("on" "Project notes" entry
-;;            (function +org-capture-central-project-notes-file)
-;;            "* %U %?\n %i\n %a"
-;;            :heading "Notes"
-;;            :prepend t)
-;;           ("oc" "Project changelog" entry
-;;            (function +org-capture-central-project-changelog-file)
-;;            "* %U %?\n %i\n %a"
-;;            :heading "Changelog"
-;;            :prepend t)))
 
 ;; Pomodoro
 (after! org
