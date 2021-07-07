36865951963ebea05ca0c63a03e45dc72acaa76a
Samuel Jackson <dsiq3g@gmail.com>
2021-07-07 11:04:47 -0700

    Removed annoying startup hooks
    Removed the startup hooks for evil-snipe and flycheck modes.

--- .doom.d/settings/evil-snipe-settings.el
+++ .doom.d/settings/evil-snipe-settings.el
@@ -4,4 +4,17 @@
 ; There can be problems between snipe mode and magit mode.
 (add-hook 'magit-mode-hook 'turn-off-evil-snipe-override-mode)
 
-(map! :map general-override-mode-map :nv "s" #'evil-substitute)
+(remove-hook! 'doom-first-input-hook 'evil-snipe-mode)
+
+(defun toggle-and-activate-evil-snipe-mode ()
+  "Toggles evil-snipe-mode on and off then activates the
+mode map since otherwise it requires forcing the normal mode state to be activated."
+  (interactive)
+  (evil-snipe-local-mode)
+  (evil-force-normal-state))
+
+(map! :leader
+      :desc "Evil snipe mode"
+      "t S" #'toggle-and-activate-evil-snipe-mode)
+; (map! :map general-override-mode-map :nv "s" #'evil-substitute)
+;; (after! evil-snipe (evil-snipe-mode -1))
