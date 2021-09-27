4c87b2e86730e72d10bacdb8c4393b89b46519f6
Samuel Jackson <dsiq3g@gmail.com>
2021-09-27 00:24:54 -0500

    Added SSL configurations for guix setup

--- .shell_setup/env_var_setup.global.sh
+++ .shell_setup/env_var_setup.global.sh
@@ -14,5 +14,12 @@ export MANPATH="/usr/local/man:$MANPATH"
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-# Guix local path
+# Guix settings
 export GUIX_LOCPATH="$HOME/.guix-profile/lib/locale"
+
+guixSSL="$HOME/.guix-profile/etc/ssl/certs${SSL_CERT_DIR:+:}$SSL_CERT_DIR"
+guixGitSSL="$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt${GIT_SSL_CAINFO:+:}$GIT_SSL_CAINFO"
+if [ -f guixSSL ]; then
+    export SSL_CERT_DIR=$guixSSL
+    export GIT_SSL_CAINFO=$guixGitSSL
+fi
