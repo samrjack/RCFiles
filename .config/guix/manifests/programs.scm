fc8150c11d1dc63b01411ebaa650a205c4e067b9
Samuel Jackson <dsiq3g@gmail.com>
2021-12-04 19:05:42 -0800

    Added more guix programs

--- .config/guix/manifests/programs.scm
+++ .config/guix/manifests/programs.scm
@@ -2,20 +2,47 @@
 (specifications->manifest
   '(
     ;;; Window managers
+    "awesome"
     "xmonad"
-    "xmobar"
     "i3-wm"
+    "dwm"
+
+    ;;; window manager dependencies
+    "xmobar"
+    "i3lock"
     "i3lock-fancy"
+    "i3lock-color"
+    "xss-lock"
     "i3blocks"
     "i3-gaps"
     "i3status"
 
+    "xbacklight"
+    "compton"
+    "nitrogen"
+    "feh"
+    "arandr"
+    "rofi"
+    "dmenu"
+    "pulseaudio"
+
+    ;;; Shells
+    "zsh"
+    "bash"
+
     ;;; Programming languages
+    "ghc" ; haskell
     "smalltalk"
     "go"
+    "clojure"
     "leinigen" ; Clojure environment
+    "python"
+    "python-pip"
+    "ruby"
+    "rust"
 
     ;;; Programming tools
+    "hoogle"
     "git"
     "vim"
     "neovim"
@@ -45,6 +72,12 @@
     ;;; CLI tools
     "ffmpeg"
     "cmatrix"
+    "nettools"
+    "pandoc"
+    "asciidoc"
+    "sed"
+    "password-store"
+    "ranger"
 
     ;; Web tools
     "wget"
