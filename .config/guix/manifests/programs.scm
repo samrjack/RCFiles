2b0ac0fde08f02b952edd6caef9c465090cb045b
Samuel Jackson <dsiq3g@gmail.com>
2021-12-04 18:52:32 -0800

    Added a manifest with a large number of programs I use regularly

---
+++ .config/guix/manifests/programs.scm
@@ -0,0 +1,52 @@
+
+(specifications->manifest
+  '(
+    ;;; Window managers
+    "xmonad"
+    "xmobar"
+    "i3-wm"
+    "i3lock-fancy"
+    "i3blocks"
+    "i3-gaps"
+    "i3status"
+
+    ;;; Programming languages
+    "smalltalk"
+    "go"
+    "leinigen" ; Clojure environment
+
+    ;;; Programming tools
+    "git"
+    "vim"
+    "neovim"
+    "emacs"
+
+    ;;; Other programming environments
+    "texlive"
+    "texmaker"
+    "octave"
+
+    ;;; GUI programs
+    "blender"
+    "gimp"
+    "inkscape"
+    "audacity"
+    "gparted"
+    "steam"
+
+    ;;; Office programs
+    "libreoffice"
+
+    ;;; Web browsers
+    "qutebrowser"
+    "firefox"
+    "nyxt"
+
+    ;;; CLI tools
+    "ffmpeg"
+    "cmatrix"
+
+    ;; Web tools
+    "wget"
+    "curl"
+    ))
