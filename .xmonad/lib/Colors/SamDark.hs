edaa9de581d2d3272a1258b608398aace9df441b
Samuel Jackson <dsiq3g@gmail.com>
2021-12-06 18:40:21 -0800

    Added colorschemes and made my own

---
+++ .xmonad/lib/Colors/SamDark.hs
@@ -0,0 +1,28 @@
+module Colors.SamDark where
+
+import XMonad
+
+colorScheme = "sam-dark"
+
+colorBack = "#344055"
+colorFore = "#d0c6f8"
+
+color01 = "#1c1f24" -- dark black
+color02 = "#c33c54" -- red
+color03 = "#62A87C" -- green
+color04 = "#FFAA43" -- orange
+color05 = "#3f88c5" -- blue
+color06 = "#621059" -- magenta
+color07 = "#829cbc" -- teal
+color08 = "#202328" -- black
+color09 = "#5b6268" -- grey
+color10 = "#da6874" -- light red
+color11 = "#9ee493" -- light green
+color12 = "#f3de8a" -- light orange
+color13 = "#3071db" -- light blue
+color14 = "#53599a" -- light magenta
+color15 = "#96e0dc" -- light teal
+color16 = "#d5d4d6" -- white
+
+colorTrayer :: String
+colorTrayer = "--tint 0x282c34"
