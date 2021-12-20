cc5a06dc14031401c78cd353c72658f8df1eee48
Samuel Jackson <dsiq3g@gmail.com>
2021-12-19 16:49:54 -0800

    Removed some comments and fixed spelling

--- configs/.doom.d/config.el
+++ configs/.doom.d/config.el
@@ -12,19 +12,6 @@
   (interactive)
   (variable-pitch-mode))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
 (setq custom-file (expand-file-name ".custom.el" doom-private-dir))
 (when (file-exists-p custom-file) (load custom-file))
 
