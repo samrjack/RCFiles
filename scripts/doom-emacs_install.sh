f01ae8e08d5aaf95d2c6a9a7646bebde46c2de3e
Samuel Jackson <dsiq3g@gmail.com>
2021-12-23 20:55:10 -0800

    Made doom emacs pull all git history

--- scripts/doom-emacs_install.sh
+++ scripts/doom-emacs_install.sh
@@ -2,6 +2,6 @@
 
 if [ ! -f "$HOME/.emacs.d/bin/doom" ]
 then
-    git clone --depth 1 https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
+    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
     "$HOME"/.emacs.d/bin/doom install
 fi
