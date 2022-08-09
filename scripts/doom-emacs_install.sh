ff1764df264393161b9235b00867c6683d74fcd2
Samuel Jackson <dsiq3g@gmail.com>
2022-08-08 17:53:02 -0700

    Emacs - Added personal dictionary generation to install script

--- scripts/doom-emacs_install.sh
+++ scripts/doom-emacs_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "$HOME/.emacs.d/bin/doom" ]
+if [ ! -f "${HOME}/.emacs.d/bin/doom" ]
 then
-    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
-    "$HOME"/.emacs.d/bin/doom install
+    git clone https://github.com/hlissner/doom-emacs "${HOME}/.emacs.d"
+    "${HOME}"/.emacs.d/bin/doom install
+
+    # Make directory for personal dictionary
+    mkdir -p ${HOME}/.emacs.d/.local/etc/ispell
+    touch ${HOME}/.emacs.d/.local/etc/ispell/en.pws
 fi
