beefcc16e8245863eb0afbdf14523121d9f13989
Samuel Jackson <dsiq3g@gmail.com>
2022-10-24 16:28:33 -0700

    Emacs - Add spray key commands

--- configs/.doom.d/config.el
+++ configs/.doom.d/config.el
@@ -208,6 +208,29 @@ mode map since otherwise it requires forcing the normal mode state to be activat
                    :height 140
                    :italic t))))
 
+(use-package! spray
+  :commands spray-mode
+  :config
+  (setq spray-wpm 600
+        spray-height 800)
+  (defun local/spray-mode-hide-cursor()
+    "Hide or unhide the cursor as is appropriate."
+    (if spray-mode
+        (setq-local spray--last-evil-cursor-state evil-normal-state-cursor
+                    evil-normal-state-cursor '(nil))
+      (setq-local evil-normal-state-cursor spray--last-evil-cursor-state)))
+  (add-hook 'spray-mode-hook #'local/spray-mode-hode-cursor)
+  (map! :map spray-mode-map
+        "<return>" #'spray-start/stop
+        "f" #'spray-faster
+        "s" #'spray-slower
+        "t" #'spray-time
+        "<right>" #'spray-forward-word
+        "h" #'spray-forward-word
+        "<left>" #'spray-backward-word
+        "l" #'spray-backward-word
+        "q" #'spray-quit))
+
 (use-package! keycast
   :commands keycast-mode
   :config
