5272f1e4fc5cc5ffd32a82dd16ce7846471ab379
Samuel Jackson <dsiq3g@gmail.com>
2023-01-22 13:34:50 -0800

    MacOS - Move path specifications into global file

--- configs/.shell_setup/path_setup.global.sh
+++ configs/.shell_setup/path_setup.global.sh
@@ -1,7 +1,9 @@
 # Setting up the path variable for all shell environments.
+# Path folders are searched in the reverse order that they are listed in this file.
 
 # program files
 export PATH="/usr/local/bin:${PATH}"
+export PATH="/usr/local/sbin:${PATH}"
 export PATH="/sbin:${PATH}"
 
 # Doom emacs
@@ -23,25 +25,29 @@ export PATH="${HOME}/.npm-global/bin:${PATH}"
 
 # Rust
 export PATH="${HOME}/.cargo/env:${PATH}"
-
-# Rust build system
 export PATH="${HOME}/.cargo/bin:${PATH}"
 
 # Haskell
 export PATH="${HOME}/.cabal/bin:${PATH}"
 
+# Brew package manager for macos
+export PATH="/opt/homebrew/bin:${PATH}"
+export PATH="/opt/homebrew/opt/openjdk/bin:${PATH}"
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}"
+
+
 # Source a GUIX environment if present
-GUIX_PROFILE="${HOME}/.guix-profile"
-GUIX_PROFILE_SCRIPT="$GUIX_PROFILE/etc/profile"
-if [ -e "$GUIX_PROFILE_SCRIPT" ]; then
-   . "$GUIX_PROFILE_SCRIPT"
+export GUIX_PROFILE="${HOME}/.guix-profile"
+export GUIX_PROFILE_SCRIPT="${GUIX_PROFILE}/etc/profile"
+if [ -e ${GUIX_PROFILE_SCRIPT} ]; then
+   . ${GUIX_PROFILE_SCRIPT}
 
    # If guix is updated, it needs to source its new location too. However,
    # if it's not updated, then we should overwrite the above variables.
    NEW_GUIX_PROFILE="${HOME}/.config/guix/current"
-   NEW_GUIX_PROFILE_SCRIPT="$NEW_GUIX_PROFILE/etc/profile"
-   if [ -e "$NEW_GUIX_PROFILE_SCRIPT" ]; then
-       GUIX_PROFILE="$NEW_GUIX_PROFILE"
-       . "$NEW_GUIX_PROFILE_SCRIPT"
+   NEW_GUIX_PROFILE_SCRIPT="${NEW_GUIX_PROFILE}/etc/profile"
+   if [ -e ${NEW_GUIX_PROFILE_SCRIPT} ]; then
+       export GUIX_PROFILE=${NEW_GUIX_PROFILE}
+       . ${NEW_GUIX_PROFILE_SCRIPT}
    fi
 fi
