f0965a5061f4ac957248bae1790e4099d9ddd302
Samuel Jackson <dsiq3g@gmail.com>
2023-01-24 09:50:54 -0800

    Emacs - Add java folding definitions
    For some reason, java isn't set up to fold correctly so I'm adding
    definitions here until I can get it fixed in ts-fold itself.

--- configs/.doom.d/config.el
+++ configs/.doom.d/config.el
@@ -290,6 +290,21 @@ RULES should be a list of folding rules in the format of (ts-element . folding-f
   (dolist (mode '(javascript-mode rjsx-mode js-mode js2-mode js3-mode))
     (local/update-ts-fold-definitions mode local/ts-fold-parsers-javascript-list)))
 
+(setq local/ts-fold-parsers-java-list
+      '((block . ts-fold-range-seq)
+       (element_value_array_initializer . ts-fold-range-seq)
+       (module_body . ts-fold-range-seq)
+       (enum_body . ts-fold-range-seq)
+       (class_body . ts-fold-range-seq)
+       (constructor_body . ts-fold-range-seq)
+       (annotation_type_body . ts-fold-range-seq)
+       (interface_body . ts-fold-range-seq)
+       (array_initializer . ts-fold-range-seq)
+       (block_comment . (ts-fold-range-seq 1 -1))))
+
+(after! ts-fold
+  (local/update-ts-fold-definitions 'java-mode local/ts-fold-parsers-java-list))
+
 (setq local/ts-fold-parsers-shell-list
       '((do_group . (ts-fold-range-seq 1 -3))
         (compound_statement . ts-fold-range-seq)
