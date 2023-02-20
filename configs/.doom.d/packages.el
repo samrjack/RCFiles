fb10c428ede8bdcb692b3ad59f3fe900c2d34217
Samuel Jackson <dsiq3g@gmail.com>
2023-02-19 17:15:22 -0800

    Emacs - Fix temporary dependency issue

--- configs/.doom.d/packages.el
+++ configs/.doom.d/packages.el
@@ -165,6 +165,9 @@
 
 (unpin! straight)
 
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab")
+
 ;; (package! ts-fold :recipe
 ;;   (:host github
 ;;    :repo "samrjack/ts-fold"
