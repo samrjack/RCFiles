d1c01fc13ab2bee998724a76f1edb235de431a48
Samuel Jackson <dsiq3g@gmail.com>
2023-11-28 15:43:09 -0800

    Env - Add homebrew variables to all shells

--- configs/.shell_setup/env_var_setup.global.sh
+++ configs/.shell_setup/env_var_setup.global.sh
@@ -6,6 +6,19 @@ export SSH_KEY_PATH="${HOME}/.ssh/rsa_id"
 
 # path to man pages
 export MANPATH="/usr/local/man:$MANPATH"
+export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}";
+
+# Homebrew variables
+if test -d "/home/linuxbrew"; then
+    export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+    export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar";
+    export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew";
+elif test -d "/opt/homebrew"; then
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+fi
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
