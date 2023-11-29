d2f454d8dfeb6a1c8cf336fa8c945ffec1b83cc7
Samuel Jackson <dsiq3g@gmail.com>
2023-11-28 16:39:38 -0800

    OMZ - Fix download option for keep zshrc

--- scripts/oh-my-zsh_install.sh
+++ scripts/oh-my-zsh_install.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d "${HOME}/.oh-my-zsh" ]
 then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 fi
