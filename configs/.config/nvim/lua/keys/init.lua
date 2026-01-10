f495c0c1f262ab93edf6347953cfdb3815fdcd9d
Samuel Jackson <dsiq3g@gmail.com>
2026-01-10 09:44:22 -0800

    Nvim - Add keys to exit terminal mode

--- configs/.config/nvim/lua/keys/init.lua
+++ configs/.config/nvim/lua/keys/init.lua
@@ -1,3 +1,4 @@
 require('keys.general')
 require('keys.shortcuts')
 require('keys.lsp')
+require('keys.terminal')
---
+++ configs/.config/nvim/lua/keys/terminal.lua
@@ -0,0 +1,4 @@
+-- Keybindings for terminal
+
+vim.keymap.set({ 't' }, '<esc>', '<C-\\><C-n>', { desc = 'Escape' })
+vim.keymap.set({ 't' }, 'jk', '<C-\\><C-n>', { desc = 'Escape' })
