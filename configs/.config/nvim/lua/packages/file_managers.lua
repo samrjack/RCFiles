c6505a7663da12c928eeba6ce6097f1b52c18984
Samuel Jackson <dsiq3g@gmail.com>
2026-01-16 13:51:28 -0800

    Nvim - Improve TAB even more by tracking files within dir

--- configs/.config/nvim/lua/packages/file_managers.lua
+++ configs/.config/nvim/lua/packages/file_managers.lua
@@ -25,12 +25,27 @@ return {
                         function(state)
                             local node = state.tree:get_node()
                             local commands = require('neo-tree.sources.filesystem.commands')
+                            local renderer = require('neo-tree.ui.renderer')
+
                             if node.type == 'directory' then
-                                commands.toggle_node(state)
+                                if not node:is_expanded() then
+                                    -- Expanding - check if we should return to previous file
+                                    commands.toggle_node(state)
+                                    if vim.w.neo_tree_last_file and vim.w.neo_tree_last_parent == node:get_id() then
+                                        renderer.focus_node(state, vim.w.neo_tree_last_file)
+                                        vim.w.neo_tree_last_file = nil
+                                        vim.w.neo_tree_last_parent = nil
+                                    end
+                                else
+                                    commands.toggle_node(state)
+                                end
                             else
+                                -- On a file - store it before collapsing parent
                                 local parent_id = node:get_parent_id()
                                 if parent_id then
-                                    require('neo-tree.ui.renderer').focus_node(state, parent_id)
+                                    vim.w.neo_tree_last_file = node:get_id()
+                                    vim.w.neo_tree_last_parent = parent_id
+                                    renderer.focus_node(state, parent_id)
                                     commands.toggle_node(state)
                                 end
                             end
